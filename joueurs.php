4e1e97e98ff81968bc2f1377128137735e81f920
ANTHO_XB <160512730+Anthony68170@users.noreply.github.com>
2024-09-11 12:17:20 +0200

    Modifs Exo FOOT PHP2 POO _ Function Ajouter Equipe

--- joueurs.php
+++ joueurs.php
@@ -3,27 +3,50 @@
 
  class Joueurs{ // Déclarer la CLASS "Joueurs" - pour lister les joueurs
     // Déclarer les ATTRIBUTS de la "class"
-    private $prenom;
-    private $nom;
-    private $paysOrigine;
-    private array $equipe; // Créer le TABLEAU pour les "équipes"
+    private string $_prenom;
+    private string $_nom;
+    private string $_paysOrigine;
+    private array $_equipes; // Créer le TABLEAU pour les "équipes"
 
 // __construct pour INIATILIASER "chaque" attributs pour "chaque" joueur
     public function __construct($prenom, $nom, $paysOrigine){
         $this -> _prenom = $prenom;
         $this -> _nom = $nom;
         $this -> _paysOrigine = $paysOrigine;
-        $this -> __equipe = $equipe = array(); // INITIALISER le TABLEAU des équipes
+        $this -> __equipes = array(); // INITIALISER le TABLEAU des équipes
     }
 
+    // Placer une METHODE pour "STOCKER" chaque équipe"
+    public function ajouterEquipes($equipes){ // AJOUTER "chaque" équipe au tableau
+        $this -> __equipes[] = $equipes ;
+    }
     //GETTERS
     public function getPrenom(){
-        return $this -> _prenom;
+        return $this -> _prenom; // Le "_" permet d'accéder aux attributs
     }
     public function getNom(){
         return $this -> _nom;
     }
-    public function -> getpaysOrigine(){
+    public function getPaysOrigine(){
         return $this -> _paysOrigine;
     }
+    public function getEquipe(){
+        return $this -> _equipes;
+    }
+
+    //SETTERS
+    public function setPrenom($prenom){
+        $this -> _prenom = $prenom;
+    }
+    public function setNom($nom){
+        $this -> _nom = $nom;
+    }
+    public function setPaysOrigine($paysOrigine){
+        $this -> _paysOrigine = $paysOrigine;
+    }
+    public function setEquipe($equipes){
+        $this -> _equipes = $equipes;
+    }
+
+
  }
--- pays.php
+++ pays.php
@@ -1,3 +1,7 @@
 <!-- CODE PAYS -->
 
 <?php
+
+class Pays{ // Déclarer la CLASS "Pays" - pour lister les Pays
+   // Déclarer les ATTRIBUTS de la "class"
+   private $prenom;
